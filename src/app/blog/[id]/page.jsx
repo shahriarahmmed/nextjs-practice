24241604573d5d70bc12685e92ddf0b5afcd1c90
shahriarahmmed <shahariarahmmed.sa@gmail.com>
2023-09-06 15:55:45 +0600

    nextjs data fetch loacl json file client side SWR

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import styles from "./page.module.css"
+import Image from 'next/image';
+import {notFound} from "next/navigation"
 
-const BlogPost = () => {
+
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  })
+  if (!res.ok) {
+    return notFound()
+  }
+  return res.json();
+}
+
+const BlogPost = async ({params}) => {
+  const data = await getData(params.id)
   return (
-    <div>BlogPost</div>
+    <div>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            {data.title}
+          </h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Unde aliquam facere 
+            ut explicabo architecto aspernatur 
+            magni cupiditate nisi, aperiam repudiandae
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/16864829/pexels-photo-16864829.jpeg?auto="
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Jhon Doe</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,62 @@
+export const items = {
+    applications: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+        },
+        {
+            id: 2,
+            title: "Minimal Single Product",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+        },
+        {
+            id: 3,
+            title: "Strong Together Charity",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        },
+    ],
+    illustration: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+        },
+        {
+            id: 2,
+            title: "Minimal Single Product",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+        },
+        {
+            id: 3,
+            title: "Strong Together Charity",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        },
+    ],
+    website: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+        },
+        {
+            id: 2,
+            title: "Minimal Single Product",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+        },
+        {
+            id: 3,
+            title: "Strong Together Charity",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        },
+    ],
+}
